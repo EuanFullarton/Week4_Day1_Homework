424f572031501c854697e804cb00796fe2937406
Euan Fullarton <euanfullarton7@gmail.com>
2017-03-27 16:14:22 +0100

    win methods now working

--- controller.rb
+++ controller.rb
@@ -3,6 +3,8 @@ require ('sinatra/contrib/all')
 require ('pry-byebug')
 require_relative ('./models/game')
 
-get '/game/:rock/:rock' do
-
+get '/play/:p1/:p2' do
+  game = Game.new(params[:p1], params[:p2])
+  @winner = game.play()
+  erb(:result)
 end
--- models/game.rb
+++ models/game.rb
@@ -5,30 +5,22 @@ class Game
     @p2 = p2
   end
 
-  def p1_rock(p1, p2)
-    if p1 == 'rock' && p2 == 'rock'
+  def play()
+    if @p1 == "rock" && @p2 == "rock"
       return "It's a draw!"
-    elsif p1 == 'rock' && p2 == 'paper'
+    elsif @p1 == "rock" && @p2 == "paper"
       return "Player 2 wins with paper!"
-    else
+    elsif @p1 == "rock" && @p2 == "scissors"
       return "Player 1 wins with rock!"
-    end
-  end
-
-  def p1_paper(p1, p2)
-    if p1 == 'paper' && p2 == 'paper'
+    elsif @p1 == "paper" && @p2 == "paper"
       return "It's a draw!"
-    elsif p1 == 'paper' && p2 == 'scissors'
+    elsif @p1 == "paper" && @p2 == "scissors"
       return "Player 2 wins with scissors!"
-    else
+    elsif @p1 = "paper" && @p2 == "rock"
       return "Player 1 wins with paper!"
-    end
-  end
-
-  def p1_scissors(p1, p2)
-    if p1 == 'scissors' && p2 == 'scissors'
+    elsif @p1 == "scissors" && @p2 == "scissors"
       return "It's a draw!"
-    elsif p1 == 'scissors' && p2 == 'rock'
+    elsif @p1 == "scissors" && @p2 == "rock"
       return "Player 2 wins with rock!"
     else
       return "Player 1 wins with scissors!"
